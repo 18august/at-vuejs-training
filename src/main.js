1afdf4b8af51eeff72e7fae05ac39ae3f0005a8d
at-minhdao <minh.dao@asiantech.vn>
2019-10-28 21:25:05 +0700

    add package

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
+import VueI18n from "vue-i18n";
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import i18n from "./locales/config";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
 
+Vue.use(VueI18n);
+
 new Vue({
   router,
+  i18n,
   store,
   render: h => h(App)
 }).$mount('#app')
